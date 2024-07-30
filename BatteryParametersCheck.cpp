3fd64bf11104f8888d9baa1a872de708962c0131
Ankushbh608 <61629794+Ankushbh608@users.noreply.github.com>
2024-07-30 22:59:03 +0530

    Update BatteryParametersCheck.cpp

--- BatteryParametersCheck.cpp
+++ BatteryParametersCheck.cpp
@@ -8,7 +8,7 @@ WarningStatus getWarningStatus(float value, const ParameterRange& range) {
     if (value >= range.highWarning) {
         return HIGH_WARNING;
     }
-    return NO_WARNING;
+    return NO_WARNING; // Normal if not in any warning range
 }
 
 BreachStatus getBreachStatus(float value, const ParameterRange& range) {
@@ -18,43 +18,58 @@ BreachStatus getBreachStatus(float value, const ParameterRange& range) {
     if (value >= range.highBreach) {
         return HIGH_BREACH;
     }
-    return NO_BREACH;
+    return NO_BREACH; // Not in breach
 }
 
-std::string statusToMessageTranslation(const std::string& parameter, BreachStatus breachStatus, WarningStatus warningStatus) {
+std::string getBreachMessage(const std::string& parameter, BreachStatus breachStatus) {
     if (breachStatus == LOW_BREACH) {
         return parameter + " is below the safe range!";
     }
     if (breachStatus == HIGH_BREACH) {
         return parameter + " is above the safe range!";
     }
+    return "";
+}
+
+std::string getWarningMessage(const std::string& parameter, WarningStatus warningStatus) {
     if (warningStatus == LOW_WARNING) {
         return "Warning: " + parameter + " is approaching discharge.";
     }
     if (warningStatus == HIGH_WARNING) {
         return "Warning: " + parameter + " is approaching charge-peak.";
     }
-    return parameter + " is normal.";
+    return "";
+}
+
+std::string statusToMessageTranslation(const std::string& parameter, BreachStatus breachStatus, WarningStatus warningStatus) {
+    std::string message = getWarningMessage(parameter, warningStatus);
+    if (message.empty()) {
+        message = getBreachMessage(parameter, breachStatus);
+    }
+    if (message.empty()) {
+        message = parameter + " is normal.";
+    }
+    return message;
 }
 
 bool checkTemperature(float temperature, BreachStatus& breachStatus, WarningStatus& warningStatus) {
     ParameterRange temperatureRange = {0, 0 + 45 * 0.05, 45 - 45 * 0.05, 45};
-    breachStatus = getBreachStatus(temperature, temperatureRange);
     warningStatus = getWarningStatus(temperature, temperatureRange);
+    breachStatus = getBreachStatus(temperature, temperatureRange);
     return breachStatus == NO_BREACH;
 }
 
 bool checkSoc(float soc, BreachStatus& breachStatus, WarningStatus& warningStatus) {
     ParameterRange socRange = {20, 20 + 80 * 0.05, 80 - 80 * 0.05, 80};
-    breachStatus = getBreachStatus(soc, socRange);
     warningStatus = getWarningStatus(soc, socRange);
+    breachStatus = getBreachStatus(soc, socRange);
     return breachStatus == NO_BREACH;
 }
 
 bool checkChargeRate(float chargeRate, BreachStatus& breachStatus, WarningStatus& warningStatus) {
     ParameterRange chargeRateRange = {0, 0, 0.8 - 0.8 * 0.05, 0.8};
-    breachStatus = getBreachStatus(chargeRate, chargeRateRange);
     warningStatus = getWarningStatus(chargeRate, chargeRateRange);
+    breachStatus = getBreachStatus(chargeRate, chargeRateRange);
     return breachStatus == NO_BREACH;
 }
 
